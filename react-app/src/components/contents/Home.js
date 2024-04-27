47fa50548ef5b449f06c09286d14dc086a5b8973
HarkHorning <147337852+HarkHorning@users.noreply.github.com>
2024-04-27 15:36:11 -0400

    Update Home.js

--- react-app/src/components/contents/Home.js
+++ react-app/src/components/contents/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from "./logoCal.png";
 import Footer from '../Footer';
+import HowDo from './contents_sections/how_do_we';
 
 function Home() {
 
@@ -25,43 +26,17 @@ function Home() {
             </header>
             <div className='page flex'>
               <div className='w-2/3 flex flex-col'>
-                <h2 className='flex mt-6 text-5xl text-yellow-500 font-light'>This is the home page:</h2>
-                <p className='text-2xl text-black mt-16 leading-8 pr-2'>
+                <h2 className='this-page flex mt-6 text-5xl font-semibold'>This is the home page:</h2>
+                <p className='page-one-contents text-black mt-12 leading-8 pr-2'>
                   A way to never miscomunicate schedules again. Seemlessly integrate the schedules of other users with your own.
                 </p>
                 
               </div>
               <img className='home-logo' src={logo} alt='logo' />
             </div>
-            <h3 className='mt-20 text-3xl text-slate-700 font-light'>How do we do it?</h3>
-            <p className='text-lg text-slate-600 mt-8 leading-8 w-4/5 font-semibold'>
-              A way to never miscomunicate schedules again. Seemlessly integrate the schedules of other users with your own.
-            </p>
-          <div className='mt-4 pt-12 w-full flex justify-between relative mb-8'>
-            <div className='outline-bg'></div>
-            <div className='vert-card'>
-              <h1 className='text-xl'>What's a Colony?</h1>
-              <ul className='pt-4'>
-                <li className='mb-2'>Each Colony is a network of combined schedules</li>
-                <li className='mb-2'>Each Colony is a network of combined schedules</li>
-                <li className='mb-2'>Each Colony is a network of combined schedules</li>
-              </ul>
-            </div>
-            <div className='vert-card'>
-              <h1 className='text-xl'>What's a Colony?</h1>
-              <ul className='pt-4'>
-                <li className='mb-2'>Each Colony is a network of combined schedules</li>
-                <li className='mb-2'>Each Colony is a network of combined schedules</li>
-                <li className='mb-2'>Each Colony is a network of combined schedules</li>
-              </ul>
-            </div>
-
-          </div>
-          <div className='compatibility bg-slate-600'>
-            <h1>compatibility</h1>
-          </div>
+          <HowDo />
           <Footer />
-        </div>
+          </div>
     </header>
   );
 }
