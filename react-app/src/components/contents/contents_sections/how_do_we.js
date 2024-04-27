22a7e0d45b3c13da3df392911ccf6e2681f699f7
HarkHorning <147337852+HarkHorning@users.noreply.github.com>
2024-04-27 15:32:53 -0400

    Create how_do_we.js

---
+++ react-app/src/components/contents/contents_sections/how_do_we.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function HowDo() {
+  return (
+    <div className='how-do-we w-full flex flex-col items-center gap-6'>
+        <h3 className='mt-20 text-3xl text-slate-700 font-light'>How do we do it?</h3>
+        <p className='text-lg text-slate-600 mt-8 leading-8 w-4/5 font-semibold'>
+          A way to never miscomunicate schedules again. Seemlessly integrate the schedules of other users with your own.
+        </p>
+      <div className='mt-4 pt-12 w-full flex justify-between relative mb-8 change-orientation'>
+        <div className='outline-bg'></div>
+        <div className='vert-card'>
+          <h1 className='text-xl'>What's a Colony?</h1>
+          <ul className='pt-4'>
+            <li className='mb-2'>Each Colony is a network of combined schedules</li>
+            <li className='mb-2'>Each Colony is a network of combined schedules</li>
+            <li className='mb-2'>Each Colony is a network of combined schedules</li>
+          </ul>
+        </div>
+            <div className='vert-card'>
+                <h1 className='text-xl'>What's a Colony?</h1>
+                <ul className='pt-4'>
+                    <li className='mb-2'>Each Colony is a network of combined schedules</li>
+                    <li className='mb-2'>Each Colony is a network of combined schedules</li>
+                    <li className='mb-2'>Each Colony is a network of combined schedules</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default HowDo;
