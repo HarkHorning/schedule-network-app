b869f50473b43b4e2722997efbc524c784a1e42f
HarkHorning <147337852+HarkHorning@users.noreply.github.com>
2024-04-27 15:34:32 -0400

    Create BeePath.js

---
+++ react-app/src/bGDecorations/BeePath.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BeePath() {
+  return (
+    <div className='bee-path'>
+        <div className='bee-path-two'>
+        </div>
+    </div>
+  );
+}
+
+export default BeePath;
